235733ec991315f63599680157e75270ced71bca
RafaelNTeixeira <rafael2003t.18@gmail.com>
2025-10-11 16:35:59 +0100

    Added script and marker library (not fully implemented)

---
+++ Assets/Scripts/MarkerObjectSpawner.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class MarkerObjectSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public struct MarkerPrefab
+    {
+        public string markerName;
+        public GameObject prefab;
+    }
+
+    public MarkerPrefab[] markerPrefabs;
+
+    private ARTrackedImageManager trackedImageManager;
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+
+    void Awake()
+    {
+        trackedImageManager = FindFirstObjectByType<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += OnTrackablesChanged;
+    }
+
+    void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= OnTrackablesChanged;
+    }
+
+    void OnTrackablesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        // When a new marker is detected
+        foreach (var trackedImage in eventArgs.added)
+        {
+            SpawnOrUpdatePrefab(trackedImage);
+        }
+
+        // When existing markers move or update
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            SpawnOrUpdatePrefab(trackedImage);
+        }
+
+        // When markers are lost
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            if (spawnedPrefabs.TryGetValue(trackedImage.referenceImage.name, out var spawned))
+            {
+                spawned.SetActive(false);
+            }
+        }
+    }
+
+    void SpawnOrUpdatePrefab(ARTrackedImage trackedImage)
+    {
+        string markerName = trackedImage.referenceImage.name;
+
+        // Find corresponding prefab
+        var prefabEntry = System.Array.Find(markerPrefabs, p => p.markerName == markerName);
+        if (prefabEntry.prefab == null) return;
+
+        GameObject spawned;
+        if (!spawnedPrefabs.TryGetValue(markerName, out spawned))
+        {
+            spawned = Instantiate(prefabEntry.prefab, trackedImage.transform.position, trackedImage.transform.rotation);
+            spawnedPrefabs[markerName] = spawned;
+        }
+
+        // Update position + visibility
+        spawned.transform.SetPositionAndRotation(trackedImage.transform.position, trackedImage.transform.rotation);
+        spawned.SetActive(trackedImage.trackingState == TrackingState.Tracking);
+    }
+}
