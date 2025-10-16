1d269ae2d4cc6425e2c3f4eae020fc1e4c548752
João Sobral <114337746+JPsobral20@users.noreply.github.com>
2025-10-16 15:14:45 +0100

    clean

--- Assets/Scripts/FoodTrigger.cs
+++ Assets/Scripts/FoodTrigger.cs
@@ -12,15 +12,12 @@ public class FoodTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // print the object name to the console when an object enters the trigger
-        Debug.Log("Objeto entrou no trigger: " + other.gameObject.name);
 
         if (other.CompareTag("Pou"))
         {
             if (!isInsidePou)
             {
                 isInsidePou = true;
-                Debug.Log("Pizza entrou em contacto com Pou — a fatia desaparece!");
                 SetVisible(false); // Since the object is a prefab, we disable renderers instead of the whole GameObject. Otherwise, it would cause the object from disappearing entirely from the AR scene.
             }
         }
@@ -33,7 +30,6 @@ public class FoodTrigger : MonoBehaviour
             if (isInsidePou)
             {
                 isInsidePou = false;
-                Debug.Log("Pizza saiu do contacto com Pou — a fatia reaparece!");
                 gameObject.SetActive(true);
                 SetVisible(true);
             }
